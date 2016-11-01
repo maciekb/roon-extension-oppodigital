ee063d702637a69c7c3d35659ae774b0e30ad8ae
Christopher Rieke <github@mail.rieke.tv>
2016-11-01 01:34:06 +0100

    Initial changes to control Rotel
    Used the roon-extension-devialet-expert scripts and adjusted it to
    communicate with Rotel Amps.

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ var DevialetExpert       = require("node-devialet-expert"),
 var devialet = { rs232: new DevialetExpert() };
 
 var roon = new RoonApi({
-    extension_id:        'com.roonlabs.devialet.expert',
-    display_name:        'Devialet Expert Volume and Source Control',
+    extension_id:        'com.crieke.rotel.amp',
+    display_name:        'Rotel Amp Volume and Source Control',
     display_version:     "1.0.0",
-    publisher:           'Roon Labs, LLC',
-    email:               'contact@roonlabs.com',
-    website:             'https://github.com/RoonLabs/roon-extension-devialet-expert',
+    publisher:           'Christopher Rieke',
+    email:               'chris@rieke.tv',
+    website:             'https://blog.rieke.tv',
 });
 
 var mysettings = roon.load_config("settings") || {
@@ -41,20 +41,19 @@ function makelayout(settings) {
         type:    "dropdown",
         title:   "Source for Convenience Switch",
         values:  [
-            { value: "USB"       },
-            { value: "AES/EBU"   },
-            { value: "Digital 1" },
-            { value: "Digital 2" },
-            { value: "Digital 3" },
-            { value: "Digital 4" },
-            { value: "Phono 2"   },
-            { value: "Line 1"    },
-            { value: "Line 2"    },
-            { value: "Phono"     },
-            { value: "Optical 1" },
-            { value: "Optical 2" },
-            { value: "HDMI"      },
-            { value: "Air"       },
+            { value: "cd"       },
+            { value: "coax1"   },
+            { value: "coax2" },
+            { value: "opt1" },
+            { value: "opt2" },
+            { value: "aux1" },
+            { value: "aux2"   },
+            { value: "tuner"    },
+            { value: "phono"    },
+            { value: "usb"     },
+            { value: "pc_usb" },
+            { value: "bal_xlr" },
+            { value: "rcd"      },
         ],
         setting: "source",
     });
@@ -90,7 +89,7 @@ roon.init_services({
 
 function setup_serial_port(port) {
     devialet.rs232.stop();
-    if (devialet.source_control) { devialet.source_control.destroy(); delete(devialet.source_control); }
+    if (devialet.source_control)   { devialet.source_control.destroy();   delete(devialet.source_control);   }
     if (devialet.volume_control)   { devialet.volume_control.destroy();   delete(devialet.volume_control);   }
 
     if (port)
@@ -106,58 +105,63 @@ setup_serial_port(mysettings.serialport);
 function ev_status(status) {
     let rs232 = devialet.rs232;
 
-    console.log("devialet rs232 status", status);
+    console.log("rotel rs232 status", status);
 
     if (status == "disconnected") {
-        svc_status.set_status("Could not connect to Devialet Expert on \"" + mysettings.serialport + "\"", true);
+        svc_status.set_status("Could not connect to Rotel Amp on \"" + mysettings.serialport + "\"", true);
         if (devialet.source_control) { devialet.source_control.destroy(); delete(devialet.source_control); }
         if (devialet.volume_control)   { devialet.volume_control.destroy();   delete(devialet.volume_control);   }
 
     } else if (status == "connected") {
-        svc_status.set_status("Connected to Devialet Expert", false);
+        svc_status.set_status("Connected to Rotel Amp", false);
         devialet.source_control = svc_source_control.new_device({
             state: {
-                display_name:     "Devialet Expert", // XXX need better less generic name -- can we get serial number from the RS232?
+                display_name:     "Rotel Amp", // XXX need better less generic name -- can we get serial number from the RS232?
                 supports_standby: true,
-                status:           !rs232.properties.power ? "standby" : (rs232.properties.source == mysettings.source ? "selected" : "deselected")
+                status:           !rs232.properties.power ? "on" : (rs232.properties.source == mysettings.source ? "selected" : "deselected")
             },
+    
             convenience_switch: function (req) {
                 rs232.set_source(mysettings.source, err => { req.send_complete(err ? "Failed" : "Success"); });
+                console.log("Setting Source!");
             },
             standby: function (req) {
                 this.state.status = "standby";
-                rs232.set_power(0, err => { req.send_complete(err ? "Failed" : "Success"); });
+                rs232.set_power("toggle", err => { req.send_complete(err ? "Failed" : "Success"); });
             }
         });
 
         devialet.volume_control = svc_volume_control.new_device({
             state: {
-                display_name: "Devialet Expert", // XXX need better less generic name -- can we get serial number from the RS232?
+                display_name: "Rotel Amp", // XXX need better less generic name -- can we get serial number from the RS232?
                 volume_type:  "db",
-                volume_min:   -97.5,
-                volume_max:   30,
+                volume_min:   1,
+                volume_max:   96,
                 volume_value: rs232.properties.volume,
-                volume_step:  0.5,
+                volume_step:  1,
                 is_muted:     !!rs232.properties.mute
             },
             set_volume: function (req, mode, value) {
+	            console.log("Setting ")
                 let newvol = mode == "absolute" ? value : (rs232.properties.volume + value);
                 if      (newvol < this.state.volume_min) newvol = this.state.volume_min;
                 else if (newvol > this.state.volume_max) newvol = this.state.volume_max;
                 rs232.set_volume(newvol, (err) => { req.send_complete(err ? "Failed" : "Success"); });
+                console.log("setting volume");
             },
             set_mute: function (req, action) {
-                rs232.set_mute(action == "on" ? 1 : (action == "off" ? 0 : "!"),
+                console.log("Req: " + req + " Action: " + action);
+                rs232.set_mute(action == "on" ? "on" : (action == "off" ? "off" : "toggle"),
                                 (err) => { req.send_complete(err ? "Failed" : "Success"); });
             }
         });
-    } else if (status == "initializing") {
-        svc_status.set_status("Connecting to Devialet Expert on \"" + mysettings.serialport + "\"", true);
-    }
+    } 
 }
 
 function ev_changed(name, val) {
     let rs232 = devialet.rs232;
+    if (name == "power" && !!rs232.properties.volume)
+        console.log("Gerät wurde eingeschaltet");
     if (name == "volume" && devialet.volume_control)
         devialet.volume_control.update_state({ volume_value: val });
     else if (name == "mute"   && devialet.volume_control)
